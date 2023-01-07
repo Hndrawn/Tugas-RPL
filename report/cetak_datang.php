49e8e1f4bb0f02c0c3cc95268ad4f7e8aaf2986a
Hndrawn <hendrawanvirgiawan0@gmail.com>
2023-01-07 12:27:23 +0800

    Tugas RPL

---
+++ report/cetak_datang.php
@@ -0,0 +1,124 @@
+<?php
+	include "../inc/koneksi.php";
+	
+	if (isset ($_POST['Cetak'])){
+	$id = $_POST['id_datang'];
+	}
+
+	$tanggal = date("m/y");
+	$tgl = date("d/m/y");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>CETAK SURAT</title>
+	<style>
+		.kiri{
+			float: left;
+			width: 15%; 
+			padding-top: 20px;
+		}
+		.kanan{
+			float: right;
+			width: 85%;
+		}
+	</style>
+</head>
+
+<body>
+	<center>
+		<div class="kiri">
+			<img src="loteng.png" alt="" srcset=""width = "65px">
+		</div>
+		<div class="kanan">
+		<h2>PEMERINTAH KABUPATEN LOMBOK TENGAH</h2>
+		<h3>KECAMATAN PRAYA BARAT DAYA
+			<br>DESA BATU JANGKIH</h3>
+		</div>
+		<p>________________________________________________________________________</p>
+
+		<?php
+			$sql_tampil = "select * from tb_datang
+			where id_datang ='$id'";
+			
+			$query_tampil = mysqli_query($koneksi, $sql_tampil);
+			$no=1;
+			while ($data = mysqli_fetch_array($query_tampil,MYSQLI_BOTH)) {
+		?>
+	</center>
+
+	<center>
+		<h4>
+			<u>SURAT KETARANGAN PENDATANG</u>
+		</h4>
+		<h4>No Surat :
+			<?php echo $data['id_datang']; ?>/Ket.Pendatang/
+			<?php echo $tanggal; ?>
+		</h4>
+	</center>
+	<p>Yang bertandatangan dibawah ini Kepala Desa ........., Kecamatan ............., Kabupaten .........., dengan ini menerangkan
+		bahawa :</P>
+	<table>
+		<tbody>
+			<tr>
+				<td>NIK</td>
+				<td>:</td>
+				<td>
+					<?php echo $data['nik']; ?>
+				</td>
+			</tr>
+			<tr>
+				<td>Nama</td>
+				<td>:</td>
+				<td>
+					<?php echo $data['nama_datang']; ?>
+				</td>
+			</tr>
+			<tr>
+				<td>Jenis Kelamin</td>
+				<td>:</td>
+				<td>
+					<?php echo $data['jekel']; ?>
+				</td>
+			</tr>
+			<tr>
+				<td>Tanggal Datang</td>
+				<td>:</td>
+				<td>
+					<?php echo $data['tgl_datang']; ?>
+				</td>
+			</tr>
+			<?php } ?>
+		</tbody>
+	</table>
+	<p>Benar-benar Telah datang dan berencana untuk tinggal di Desa Batu Jangkih, Kecamatan Praya Barat Daya, Kabupuaten Lombok Tengah.</P>
+	<p>Demikian Surat ini dibuat, agar dapat digunakan sebagaimana mestinya.</P>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<p align="right">
+		Batu Jangkih,
+		<?php echo $tgl; ?>
+		<br> KEPALA DESA ...............
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>(....................................................)
+	</p>
+
+
+	<script>
+		window.print();
+	</script>
+
+</body>
+
+</html>
